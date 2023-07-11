d4d8ea0d573208a8ca4bb6ec0a7c898f498733c1
addisu30 <105362027+addisu30@users.noreply.github.com>
2023-07-11 20:46:18 +0300

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""async_generator that takes no arguments."""
-import random
-import asyncio
+
+""" Async Comprehensions """
+
+from asyncio import sleep
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """
-    async_generator - function to loop 10 times
-    random number between 0 and 10. Use the random module
-    """
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+    """ Async Generator """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
